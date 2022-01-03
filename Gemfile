91cdd5f7d3950561e2b37749ed4a5b772de2e5d8
shibu55 <segusuigao@gmail.com>
2022-01-03 18:29:08 +0900

    iroiro

--- Gemfile
+++ Gemfile
@@ -57,7 +57,6 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 
 gem 'devise'
-gem 'pry-rails'
 gem 'jquery-rails'
 gem 'simple_calendar', '~> 2.0'
 gem 'pry-rails'
--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -4,11 +4,32 @@ class HolidaysController < ApplicationController
 
   def new
     @holiday = Holiday.new
+    @holidays = Holiday.all
   end
 
   def create
     Holiday.create(holiday_parameter)
-    redirect_to root_path
+    redirect_to new_holiday_path
+  end
+
+  def destroy
+    @holiday = Holiday.find(params[:id])
+    @holiday.destroy
+    redirect_to new_holiday_path
+  end
+
+  def edit
+    @holiday = Holiday.find(params[:id])
+  end
+
+  def update
+    binding.pry
+    @holiday = Holiday.find(params[:id])
+    if @holiday.update(holiday_parameter)
+      redirect_to new_holiday_path
+    else
+      render 'edit'
+    end
   end
 
   private
