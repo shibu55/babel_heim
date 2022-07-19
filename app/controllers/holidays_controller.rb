7e28d204d27d41d711d9bff01b5a1bb86874acde
shibu55 <segusuigao@gmail.com>
2022-07-19 12:19:01 +0900

    座席調整、定休日追加ボタン、タイトルセレクト

--- app/controllers/holidays_controller.rb
+++ app/controllers/holidays_controller.rb
@@ -12,6 +12,25 @@ class HolidaysController < ApplicationController
     redirect_to new_holiday_path
   end
 
+  def create_regular_holidays
+    current_year = Time.current.year
+    wmonth = params[:month].to_i
+    wday = params[:day].to_i
+    wdays = [:sunday,:monday,:tuesday,:wednesday,:thursday,:friday,:saturday]
+    first = Time.local(current_year, wmonth)
+    if first.wday == wday
+      target_day = first
+    else
+      target_day = first.next_occurring(wdays[wday])
+    end
+    while target_day.month == wmonth do
+      Holiday.create({title: "定休日", start_time: target_day})
+      target_day = target_day.next_occurring(wdays[wday])
+    end
+
+    redirect_to new_holiday_path
+  end
+
   def destroy
     @holiday = Holiday.find(params[:id])
     @holiday.destroy
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   }
   resources :seats, only: [:index, :create, :update]
   put 'all_seats', to: 'seats#update_all', as: 'all_seats'
+  post 'regular', to: 'holidays#create_regular_holidays', as: 'regular_holidays'
   resources :holidays
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
